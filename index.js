4c9827bb074d4290d1407c3cac26fea0f73f20db
VinayPrajapati04 <vp301334@gmail.com>
2023-04-15 04:14:33 +0530

    added New Fils

---
+++ index.js
@@ -0,0 +1,54 @@
+// function for Resume 
+
+function GetResume() {
+    
+    window.location.href="./document/FinalVinayResume.pdf"
+    }
+
+// function for optimizely
+   function optimizelyClickDeploy() {
+
+    window.location.href="https://funny-kataifi-02e4e4.netlify.app/index.html"
+ }
+
+   function optimizelyClickGithub() {
+
+   window.location.href="https://github.com/BhavikDholu/optimizely.com-clone"
+    }
+
+// function for shopify
+
+   function shopifyClickDeploy() {
+
+  window.location.href="https://illustrious-feeling-7979.vercel.app/"
+  }
+  
+  function shopifyClickGithub() {
+      
+      window.location.href="https://github.com/YAKSHITH027/illustrious-feeling-7979"
+      }
+
+      // function for ApnaCart
+
+  function ApnaCartClickDeploy() {
+
+      window.location.href="https://admirable-monstera-8dc834.netlify.app/"
+      }
+          
+  function ApnaCartClickGithub() {
+      
+      window.location.href="https://github.com/VinayPrajapati04/APNA-STORE"
+      }
+
+
+                  // function for NordStrom
+
+function NordStromClickDeploy() {
+
+  window.location.href="https://melodic-stroopwafel-d5ba73.netlify.app/"
+  }
+  
+  function NordStromClickGithub() {
+      
+      window.location.href="https://github.com/srock2580/red-sail-4113"
+  }
