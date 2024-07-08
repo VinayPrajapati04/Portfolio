9aebd4ac0177640d89a4d73233cd38fc2291717f
VinayPrajapati04 <vp301334@gmail.com>
2024-07-08 15:04:20 +0530

    added some change

--- index.js
+++ index.js
@@ -1,81 +1,68 @@
-// function for Resume 
+// function for Resume
 
 function GetResume() {
-    
-    window.location.href="https://drive.google.com/file/d/1YMoacJRh9lXASvOKlMmDqhyVGc6tsEyU/view?usp=sharing"
-    }
+  window.location.href =
+    "https://drive.google.com/file/d/1YMoacJRh9lXASvOKlMmDqhyVGc6tsEyU/view?usp=sharing";
+}
 
 // function for optimizely
-   function optimizelyClickDeploy() {
+function optimizelyClickDeploy() {
+  window.location.href = "https://optimizely-com-clone.vercel.app/";
+}
 
-    window.location.href="https://optimizely-com-clone.vercel.app/"
- }
-
-   function optimizelyClickGithub() {
-
-   window.location.href="https://github.com/BhavikDholu/optimizely.com-clone"
-    }
+function optimizelyClickGithub() {
+  window.location.href = "https://github.com/BhavikDholu/optimizely.com-clone";
+}
 
 // function for shopify
 
-   function shopifyClickDeploy() {
+function shopifyClickDeploy() {
+  window.location.href = "https://illustrious-feeling-7979.vercel.app/";
+}
 
-  window.location.href="https://illustrious-feeling-7979.vercel.app/"
-  }
-  
-  function shopifyClickGithub() {
-      
-      window.location.href="https://github.com/YAKSHITH027/illustrious-feeling-7979"
-      }
+function shopifyClickGithub() {
+  window.location.href =
+    "https://github.com/YAKSHITH027/illustrious-feeling-7979";
+}
 
-      // function for ApnaCart
+// function for ApnaCart
 
-  function ApnaCartClickDeploy() {
+function ApnaCartClickDeploy() {
+  window.location.href = "https://apna-cart-three.vercel.app/";
+}
 
-      window.location.href="https://apna-cart-three.vercel.app/"
-      }
-          
-  function ApnaCartClickGithub() {
-      
-      window.location.href="https://github.com/VinayPrajapati04/APNA-STORE"
-      }
+function ApnaCartClickGithub() {
+  window.location.href = "https://github.com/VinayPrajapati04/APNA-STORE";
+}
 
-      // function for MovieStar website
+// function for MovieStar website
 
+function MovieStarCartClickDeploy() {
+  window.location.href = "https://mini-project-3rd-year.vercel.app//";
+}
 
-  function MovieStarCartClickDeploy() {
-
-    window.location.href="https://mini-project-3rd-year.vercel.app//"
-    }
-        
 function MovieStarClickGithub() {
-    
-    window.location.href="https://github.com/VinayPrajapati04/MiniProject_3rd_Year"
-    }
+  window.location.href =
+    "https://github.com/VinayPrajapati04/MiniProject_3rd_Year";
+}
 
-
-  // function for NordStrom
+// function for NordStrom
 
 function NordStromClickDeploy() {
+  window.location.href = "https://nord-strom-project.vercel.app/";
+}
 
-  window.location.href="https://nord-strom-project.vercel.app/"
-  }
-  
-  function NordStromClickGithub() {
-      
-      window.location.href="https://github.com/srock2580/red-sail-4113"
-  }
+function NordStromClickGithub() {
+  window.location.href = "https://github.com/srock2580/red-sail-4113";
+}
 
-
-
-    // function for AVNICOMPUTER SOLUTION
+// function for AVNICOMPUTER SOLUTION
 
 function AvnicomputerClickDeploy() {
+  window.location.href = "https://avni-computer-solution-new.vercel.app/";
+}
 
-    window.location.href="https://www.avnicomputersolution.com/"
-    }
-    
-    function AvnicomputerClickGithub() {
-        
-        window.location.href="https://github.com/VinayPrajapati04/AvniComputerSolutionNew"
-    }
+function AvnicomputerClickGithub() {
+  window.location.href =
+    "https://github.com/VinayPrajapati04/AvniComputerSolutionNew";
+}
